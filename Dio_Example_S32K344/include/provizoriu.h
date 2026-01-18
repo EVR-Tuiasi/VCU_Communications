9f34a12cdc6ac9c48d365a56fd78a45c7c99a0d0
MateiVolo05 <volo.matei@gmail.com>
2026-01-18 20:25:51 +0200

    corectie extragere date CAN

--- Dio_Example_S32K344/include/provizoriu.h
+++ Dio_Example_S32K344/include/provizoriu.h
@@ -29,6 +29,57 @@
 #define INVERTERS_MOTORS_TEMPERATURE 22
 #define ERROR 100
 
+struct dateInvertoare{
+	struct{
+		uint8 tmpMotor;
+		uint8 tmpInvertor;
+		uint8 throttleSignalFeedback;
+		uint16 curent;
+		uint16 rpm;
+		uint16 tensiune;
+	}st;
+	struct{
+		uint8 tmpMotor;
+		uint8 tmpInvertor;
+		uint8 throttleSignalFeedback;
+		uint16 curent;
+		uint16 rpm;
+		uint16 tensiune;
+	}dr;
+};
+
+struct dateFrana{
+	uint8 presiune;
+	uint16 tensiuneSenzorPresiune;
+	uint16 tensiune2;
+	uint16 tensiune1;
+	uint8 cursa2;
+	uint8 cursa1;
+	uint8 erori;
+};
+
+struct dateAcceleratie{
+	uint16 tensiune2;
+	uint16 tensiune1;
+	uint8 cursa2;
+	uint8 cursa1;
+	uint8 erori;
+};
+
+struct dateBaterie{
+	uint16 curent;
+	uint16 tensiuneTSAC;
+	uint16 tmpMax;
+	uint16 tensiuneMaxCelule;
+};
+
+struct dateBord{
+	bool activationLogic;
+	bool directieMasina;
+	uint8 eroriI2C;
+	//de completat
+};
+
 bool primesteDate(Can_HwHandleType handle, Can_IdType id, PduLengthType length, uint8* data);
 
 #endif
--- Dio_Example_S32K344/src/provizoriu.c
+++ Dio_Example_S32K344/src/provizoriu.c
@@ -2,64 +2,63 @@
 
 extern uint8 UART_Channel;
 
+struct dateInvertoare invt;
+struct dateFrana frana;
+struct dateAcceleratie acceleratie;
+struct dateBaterie baterie;
+struct dateBord bord;
+
 boolean primesteDate(Can_HwHandleType handle, Can_IdType id, PduLengthType length, uint8* data){
 	//Uart_SyncSend(UART_Channel, buffer, length, 10000000);
-	uint8 buffer[length];
 	if((id&MASK)==idFrana){
 		//extragere date
-		uint8 presiune = ((data>>8) | (1<<8));
-		uint16 tensiuneSenzorPresiune = ((data>>16) | (1<<16)) & (0x01FF);
-		uint16 tensiune2 = ((data>>25) | (1<<16)) & (0x01FF);
-		uint16 tensiune1 = ((data>>34) | (1<<16)) & (0x01FF);
-		uint8 cursa2 = ((data>>43) | (1<<8)) & (0x7F);
-		uint8 cursa1 = ((data>>50) | (1<<8)) & (0x7F);
-		uint8 erori = ((data>>57) | (1<<8)) & (0x7F);
-		//trimitere date
+		frana.presiune = data[1];
+		frana.tensiuneSenzorPresiune = ((((uint16)data[3])<<8) | data[2]) & (0x01FF);
+		frana.tensiune2 = (((((uint16)data[4])<<8) | data[3]) >> 1) & (0x01FF);
+		frana.tensiune1 = (((((uint16)data[5])<<8) | data[4]) >> 2) & (0x01FF);
+		frana.cursa2 = (uint8)((((((uint16)data[6])<<8) | data[5]) >> 3) & (0x007F));
+		frana.cursa1 = (uint8)((((((uint16)data[7])<<8) | data[6]) >> 2) & (0x007F));
+		frana.erori = data[7] >> 1;
 	}
 	else if((id&MASK)==idAcceleratie){
 		//extragere date
-		uint16 tensiune2 = ((data>>25) | (1<<16)) & (0x01FF);
-		uint16 tensiune1 = ((data>>34) | (1<<16)) & (0x01FF);
-		uint8 cursa2 = ((data>>43) | (1<<8)) & (0x7F);
-		uint8 cursa1 = ((data>>50) | (1<<8)) & (0x7F);
-		uint8 erori = ((data>>57) | (1<<8)) & (0x7F);
-		//trimitere date
+		acceleratie.tensiune2 = (((((uint16)data[4])<<8) | data[3]) >> 1) & (0x01FF);
+		acceleratie.tensiune1 = (((((uint16)data[5])<<8) | data[4]) >> 2) & (0x01FF);
+		acceleratie.cursa2 = (uint8)((((((uint16)data[6])<<8) | data[5]) >> 3) & (0x007F));
+		acceleratie.cursa1 = (uint8)((((((uint16)data[7])<<8) | data[6]) >> 2) & (0x007F));
+		acceleratie.erori = data[7] >> 1;
 	}
 	else if((id&MASK)==idInvertorSt){
 		//extragere date
-		uint8 tmpMotor = data | (1<<8);
-		uint8 tmpInvertor = (data>>8) | (1<<8);
-		uint8 throttleSignalFeedback = (data>>16) | (1<<8);
-		uint16 curent = (data>>24) | (1<<16);
-		uint16 rpm = (data>>40 | (1<<16)) & (0x1FFF);
-		uint16 tensiune = (data>>53);
-		//trimitere date UART
+		invt.st.tmpMotor = data[7];
+		invt.st.tmpInvertor = data[6];
+		invt.st.throttleSignalFeedback = data[5];
+		invt.st.curent = (((uint16)data[3])<<8) | data[4];
+		invt.st.rpm = ((((uint16)data[1])<<8) | data[2]) & (0x1FFF);
+		invt.st.tensiune = ((((uint16)data[0])<<8) | data[1]) >> 5;
 	}
 	else if((id&MASK)==idInvertorDr){
 		//extragere date
-		uint8 tmpMotor = data | (1<<8);
-		uint8 tmpInvertor = (data>>8) | (1<<8);
-		uint8 throttleSignalFeedback = (data>>16) | (1<<8);
-		uint16 curent = (data>>24) | (1<<16);
-		uint16 rpm = (data>>40 | (1<<16)) & (0x1FFF);
-		uint16 tensiune = (data>>53);
-		//trimitere date UART
+		invt.dr.tmpMotor = data[7];
+		invt.dr.tmpInvertor = data[6];
+		invt.dr.throttleSignalFeedback = data[5];
+		invt.dr.curent = (((uint16)data[3])<<8) | data[4];
+		invt.dr.rpm = ((((uint16)data[1])<<8) | data[2]) & (0x1FFF);
+		invt.dr.tensiune = ((((uint16)data[0])<<8) | data[1]) >> 5;
 	}
 	else if((id&MASK)==idBaterie){
 		//extragere date
-		uint16 curent = data | (1<<16);
-		uint16 tensiuneTSAC = (data>>16) | (1<<16);
-		uint16 tmpMax = (data>>32) | (1<<16);
-		uint16 tensiuneMaxCelule = (data>>48) | (1<<16);
-		//trimitere date
+		baterie.curent = ((((uint16)data[6])<<8) | data[7]) & (0x1FFF);
+		baterie.tensiuneTSAC = ((((uint16)data[4])<<8) | data[5]) & (0x07FF);
+		baterie.tmpMax = ((((uint16)data[2])<<8) | data[3]) & (0x03FF);
+		baterie.tensiuneMaxCelule = ((((uint16)data[0])<<8) | data[1]) & (0x03FF);
 	}
 	else if((id&MASK)==idBord){
 		//extragere date
-		bool activationLogic = data & (1ULL<<63);
-		bool directieMasina = data & (1ULL<<62);
-		uint8 eroriI2C = (data>>59) | (1<<8);
+		bord.activationLogic = data[7] & (1<<7);
+		bord.directieMasina = data[7] & (1<<6);
+		bord.eroriI2C = (data[7] >> 3) & (0x0007);
 		//Mai e de completat
-		//trimitere date
 	}
 	return TRUE;
 }
