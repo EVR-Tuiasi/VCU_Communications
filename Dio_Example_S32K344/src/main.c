4944c6707931377a55c4d3870489c5f0be4b98db
MateiVolo05 <volo.matei@gmail.com>
2026-03-09 13:55:03 +0200

    Update pt testarea datelor

--- Dio_Example_S32K344/src/main.c
+++ Dio_Example_S32K344/src/main.c
@@ -64,8 +64,12 @@ extern "C" {
 /*==================================================================================================
 *                                       GLOBAL FUNCTIONS
 ==================================================================================================*/
-
-uint8_t dataDeTrimis[8]={0x2A, 0x07, 0x2B, 0x23, 0x2F, 0x23, 0x3E, 0x80};
+uint8_t dataDeTrimisInvStDr[8]={0xBB, 0x7F, 0x08, 0xFA, 0xFB, 0x32, 0xFD, 0xFE}; //invertoare stanga si dreapta
+uint8_t dataDeTrimisInv[8]={0x80, 0x00, 0x00, 0x00, 0x00, 0xFa, 0x0E, 0xD9}; //invert
+uint8_t dataDeTrimisBord[8]={0xD0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; //bord
+uint8_t dataDeTrimisAcc[8]={0x54, 0x07, 0x2B, 0x23, 0x2F, 0x23, 0x3E, 0x80}; //acceleratie
+uint8_t dataDeTrimisFrana[8]={0x54, 0x03, 0x2B, 0x23, 0x2F, 0x23, 0x3E, 0x80}; //frana
+uint8_t dataDeTrimisTSAC[8]={0x00, 0x00, 0x0F, 0x67, 0x40, 0xBB, 0xBD, 0xE8}; //TSAC
 
 int main(void)
 {
@@ -82,16 +86,54 @@ int main(void)
 	Platform_Init(NULL_PTR);
 	CanMessaging_Init();
 	UartMessaging_Init();
-
-	Can_PduType pduInfo;
-	pduInfo.swPduHandle=0;
-	pduInfo.length=8;
-	pduInfo.sdu=dataDeTrimis;
-	pduInfo.id=0x330 | 0x80000000U;
-	Can_43_FLEXCAN_Write(CAN_HTH_HANDLE, &pduInfo);
 	//UartMessaging_SetValue(Uart_TSAC_OverallCurrent, 123);
 	//Uart_SyncSend(UART_Channel, dataDeTrimis, 10, 10000000);
-	volatile int i;
+	Can_PduType pduInfoInvSt;
+	pduInfoInvSt.swPduHandle=0;
+	pduInfoInvSt.length=8;
+	pduInfoInvSt.sdu=dataDeTrimisInvStDr;
+	pduInfoInvSt.id=0x110 | 0x80000000U;
+
+	Can_PduType pduInfoInvDr;
+	pduInfoInvDr.swPduHandle=0;
+	pduInfoInvDr.length=8;
+	pduInfoInvDr.sdu=dataDeTrimisInvStDr;
+	pduInfoInvDr.id=0x111 | 0x80000000U;
+
+	Can_PduType pduInfoInv;
+	pduInfoInv.swPduHandle=0;
+	pduInfoInv.length=8;
+	pduInfoInv.sdu=dataDeTrimisInv;
+	pduInfoInv.id=0x112 | 0x80000000U;
+
+
+	Can_PduType pduInfoBord;
+	pduInfoBord.swPduHandle=0;
+	pduInfoBord.length=8;
+	pduInfoBord.sdu=dataDeTrimisBord;
+	pduInfoBord.id=0x113 | 0x80000000U;
+
+	Can_PduType pduInfoAcc;
+	pduInfoAcc.swPduHandle=0;
+	pduInfoAcc.length=8;
+	pduInfoAcc.sdu=dataDeTrimisAcc;
+	pduInfoAcc.id=0x330 | 0x80000000U;
+
+	Can_PduType pduInfoFrana;
+	pduInfoFrana.swPduHandle=0;
+	pduInfoFrana.length=8;
+	pduInfoFrana.sdu=dataDeTrimisFrana;
+	pduInfoFrana.id=0x331 | 0x80000000U;
+
+	Can_PduType pduInfoTSAC;
+	pduInfoTSAC.swPduHandle=0;
+	pduInfoTSAC.length=8;
+	pduInfoTSAC.sdu=dataDeTrimisTSAC;
+	pduInfoTSAC.id=0x114 | 0x80000000U;
+
+	Can_43_FLEXCAN_Write(CAN_HTH_HANDLE, &pduInfoFrana);
+
+	volatile i;
 	while(1){
 		UartMessaging_Update();
 		i=400000;
