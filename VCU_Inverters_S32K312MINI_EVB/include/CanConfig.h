a07dae79ae4ba30cb61257d023fbb3a1c40a251a
MateiVolo05 <volo.matei@gmail.com>
2026-06-30 00:01:09 +0300

    Config

--- VCU_Inverters_S32K312MINI_EVB/include/CanConfig.h
+++ VCU_Inverters_S32K312MINI_EVB/include/CanConfig.h
@@ -2,8 +2,8 @@
 #define CAN_CONFIG_H_
 
 //Can 0
-#define CAN_Channel1_En 88
-#define CAN_Channel1_Stb_N 85
+#define CAN_Channel1_En 85
+#define CAN_Channel1_Stb_N 84
 //Can 1
 #define CAN_Channel2_En 119
 #define CAN_Channel2_Stb_N 98
